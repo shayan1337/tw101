5d132fa77ee1e05523690490723e2f02db210ed3
shayan1337 <shayan1337@gmail.com>
2018-08-03 02:45:41 +0530

    Fizzbuzz program created

---
+++ src/FizzBuzzApp.java
@@ -0,0 +1,38 @@
+
+class FizzBuzz{
+
+    int fizzNumber;
+    int buzzNumber;
+
+    FizzBuzz(int fizzNumber, int buzzNumber){
+        this.fizzNumber = fizzNumber;
+        this.buzzNumber = buzzNumber;
+    }
+
+    void play(){
+
+        for(int number=1; number <= 100; number++)
+        {
+            if(number % fizzNumber == 0 && number % buzzNumber == 0)
+                System.out.println("FizzBuzz");
+
+            else if (number % 3 == 0)
+                System.out.println("Fizz");
+
+            else if (number % 5 == 0)
+                System.out.println("Buzz");
+
+            else
+                System.out.println(number);
+        }
+    }
+}
+
+
+public class FizzBuzzApp{
+    public static void main(String args[]){
+
+        FizzBuzz fizzbuzz = new FizzBuzz(3,5);
+        fizzbuzz.play();
+    }
+}
