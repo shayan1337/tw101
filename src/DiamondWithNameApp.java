f4f70ea9c43218f99d9ceb22c7bf7e0645fa4345
shayan1337 <shayan1337@gmail.com>
2018-08-03 12:18:51 +0530

    Diamond with name program created

---
+++ src/DiamondWithNameApp.java
@@ -0,0 +1,48 @@
+class DiamondWithName{
+
+    int height;
+    char character;
+    String name;
+
+    DiamondWithName(int height, char character, String name){
+
+        this.height = height;
+        this.character = character;
+        this.name = name;
+    }
+
+    void draw(){
+
+        char space = ' ';
+        for(int line=0; line < height-1; line++){
+
+            for(int spaces=0; spaces < height-line-1; spaces++)
+                System.out.print(space);
+
+            for(int characters = 0; characters < (2*line)+1; characters++)
+                System.out.print(character);
+
+            System.out.println();
+        }
+
+        System.out.println(name);
+
+        for(int line = height-2; line >= 0; line--)
+        {
+            for(int spaces=0; spaces < height - line -1; spaces++)
+                System.out.print(space);
+
+            for(int characters = 0; characters < (2*line)+1; characters++)
+                System.out.print(character);
+
+            System.out.println();
+        }
+    }
+}
+
+class DiamondWithNameApp{
+    public static void main(String args[]){
+        DiamondWithName diamondWithName = new DiamondWithName(10,'*',"Sayan");
+        diamondWithName.draw();
+    }
+}
