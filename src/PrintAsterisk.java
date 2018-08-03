58c98869cd8526e1dee68248fccd6be657241caa
shayan1337 <shayan1337@gmail.com>
2018-08-03 13:12:38 +0530

    Asterisk program created

---
+++ src/PrintAsterisk.java
@@ -0,0 +1,9 @@
+/* Disclaimer : Smartest code ever written by me */
+public class PrintAsterisk{
+
+    public static void main(String args[]) {
+
+        String asterisk = "*";
+        System.out.println(asterisk);
+    }
+}
