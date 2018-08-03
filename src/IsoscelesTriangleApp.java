a7fe664c4a3eb31aad6b74bcbe989ea6f1278b9f
shayan1337 <shayan1337@gmail.com>
2018-08-03 12:35:59 +0530

    Isosceles triangle program created

---
+++ src/IsoscelesTriangleApp.java
@@ -0,0 +1,32 @@
+class IsoscelesTriangle{
+
+    int height;
+    char character;
+
+    IsoscelesTriangle(int height, char character){
+        this.height = height;
+        this.character = character;
+    }
+
+    void draw(){
+
+        char space = ' ';
+        for(int line=0; line < height; line++){
+
+            for(int spaces=0; spaces < height-line-1; spaces++)
+                System.out.print(space);
+
+            for(int characters = 0; characters < (2*line)+1; characters++)
+                System.out.print(character);
+
+            System.out.println();
+        }
+    }
+}
+
+class IsoscelesTriangleApp{
+    public static void main(String args[]){
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(5,'*');
+        isoscelesTriangle.draw();
+    }
+}
